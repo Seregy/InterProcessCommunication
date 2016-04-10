25edc85fc552ec3b10732fcddf0dba96c2305aa7
Seregy <seregy77@gmail.com>
2016-04-10 23:23:10 +0300

    Moved multiplying task to sockets

--- tcpServer/client.js
+++ tcpServer/client.js
@@ -2,17 +2,27 @@ var api = {};
 global.api = api;
 api.net = require('net');
 
+var sIp = '127.0.0.1';
+var sPort = 2000;
+
 var socket = new api.net.Socket();
-var user;
+var result = [];
 
 socket.connect({
-  port: 2000,
-  host: '127.0.0.1',
+  port: sPort,
+  host: sIp,
 }, function() {
-  socket.write('Hello from client');
+  console.log('Connected to server');
   socket.on('data', function(data) {
-    user = JSON.parse(data);
-    console.log('Data received (by client): ' + data);
-    console.log('Age of ' + user.name + ' is ' + user.age);
+    console.log('Data received: ' + data);
+    data = JSON.parse(data);
+    for (var i = 0; i < data.length; i++) {
+      multiplyByTwo(data[i]);
+    }
+    socket.write(JSON.stringify(result));
   });
 });
+
+function multiplyByTwo(number) {
+  result.push(number * 2);
+}
--- tcpServer/server.js
+++ tcpServer/server.js
@@ -2,12 +2,17 @@ var api = {};
 global.api = api;
 api.net = require('net');
 
-var user = { name: 'Marcus Aurelius', age: 1895 };
+var port = 2000;
+
+var task = [2, 17, 3, 2, 5, 7, 15, 22, 1, 14, 15, 9, 0, 11];
+var results = [];
 
 var server = api.net.createServer(function(socket) {
-  socket.write(JSON.stringify(user));
+  socket.write(JSON.stringify(task));
   console.log('Connected: ' + socket.localAddress);
   socket.on('data', function(data) {
-    console.log('Data received (by server): ' + data);
+    console.log('Data received: ' + JSON.parse(data));
   });
-}).listen(2000);
+}).listen(port);
+
+console.log('Server is running at port ' + port);
